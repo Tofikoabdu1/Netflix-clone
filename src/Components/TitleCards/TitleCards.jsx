73a5f70ffe1b7510cbbf890af3c46524f1c0be04
Tofikoabdu1 <165275773+Tofikoabdu1@users.noreply.github.com>
2025-07-16 22:46:39 +0300

    Added main parts

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
-const TitleCards = () => {
-  return <div className="titlecards"></div>;
+import cards_data from "../../assets/cards/Cards_data";
+
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODdiNjc1YTIyZDVkMzc4NTI3ZWNmOTY3ZDNhZDNjMiIsIm5iZiI6MTc1MjY4NjY1MC40NjIwMDAxLCJzdWIiOiI2ODc3ZTAzYTI1YTRkOTQxM2U2YWI0NGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.qZZUhODkOLZY5zHJeHg6lhMiFpFQPFBqgQH9XyQKuZ0",
+    },
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault;
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+  return (
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
